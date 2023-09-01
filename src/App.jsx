8fe1b8c6cc2c974d8dd22906f7d0f7d581007fb8
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-01 14:51:58 +0300

    first commit (added vite react js component)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<Main />
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
+const Main = () => {
+    return (
+        <div className='pt-10 px-20 flex'>
+            <div className='flex-1'>
+                <h1>Get in touch with us</h1>
+                <p>We love chocolate chip cookies as much as you do! Our team is always experimenting with new recipes to create the perfect chocolate chip cookie. Whether you like them soft and chewy or crispy and crunchy, we've got a cookie for you. Come visit us and try one (or more!) of our delicious chocolate chip cookies.</p>
+            </div>
+            <img src='https://i.postimg.cc/m2H8k2XF/cookies.png' alt='cookies-main' />
+        </div>
+    )
+}
+
+export default Main
